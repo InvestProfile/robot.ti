39e01945f2f1d823b1bd1b82864aa9e3cf25ca64
milomory <milomory@gmail.com>
2020-04-24 06:02:09 +0300

    Че за нах

--- app/huita/Frame.js
+++ app/huita/Frame.js
@@ -7,11 +7,11 @@ exports.getFrame = async (settings, api, i) => {
 
     const OrderBook = await api.orderbookGet({ figi, depth: settings.depth });
     console.log('________________________________ Set DOM: ' + i + ' ________________________________\n');
-    console.log(await Dom.setDom({bids: OrderBook.bids, asks: OrderBook.asks, ticker: settings.ticker, i}));
+    await Dom.setDom({bids: OrderBook.bids, asks: OrderBook.asks, ticker: settings.ticker, i});
     console.log('________________________________ End Frame: ' + i + ' ________________________________\n');
 
     console.log('________________________________ Set OrderBook: ' + i + ' ________________________________\n');
-    console.log(await Dom.setOrderBook({OrderBook, ticker: settings.ticker, i}));
+    await Dom.setOrderBook({OrderBook, ticker: settings.ticker, i});
     console.log('________________________________ End OrderBook: ' + i + ' ________________________________\n');
 
     return {
