de280c17d097f7bc0eca628a0c01de238983e92d
milomory <milomory@gmail.com>
2020-04-24 06:20:30 +0300

    Че за нах

--- app/huita/Frame.js
+++ app/huita/Frame.js
@@ -5,13 +5,14 @@ exports.getFrame = async (settings, api, i) => {
 
     const { figi } = await api.searchOne({ ticker: settings.ticker });
 
+    //OrderBook
     const OrderBook = await api.orderbookGet({ figi, depth: settings.depth });
     console.log('________________________________ Set DOM: ' + i + ' ________________________________\n');
     await Dom.setDom({bids: OrderBook.bids, asks: OrderBook.asks, ticker: settings.ticker, i});
     console.log('________________________________ End Frame: ' + i + ' ________________________________\n');
 
     console.log('________________________________ Set OrderBook: ' + i + ' ________________________________\n');
-    await Dom.setOrderBook({OrderBook, ticker: settings.ticker, i});
+    await Dom.setOrderBook({orderBook: OrderBook, ticker: settings.ticker, i});
     console.log('________________________________ End OrderBook: ' + i + ' ________________________________\n');
 
     return {
--- app/models/orderBook.model.js
+++ app/models/orderBook.model.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, Sequelize) => {
         ticker: {
             type: Sequelize.STRING
         },
-        data: {
+        orderBook: {
             type: Sequelize.JSON
         }
     });
