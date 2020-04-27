d5ebafef3dfce8a19c51ab5593e7c110010ffd89
milomory <milomory@gmail.com>
2020-04-27 09:32:45 +0300

    minor

--- app/controllers/dom.controller.js
+++ app/controllers/dom.controller.js
@@ -4,21 +4,11 @@ const { Dom } = require('../models');
 
 // Get Dom by DomId
 router.get('/:id', async (req, res) => {
-    Dom.findOne({where: {id: req.params.id}}).then(dom => res.send({
-        url: 'dom',
-        status: 200,
-        dom
-    }))
+    Dom.findOne({where: {id: req.params.id}}).then(dom => res.send(dom))
 });
 // Get all Doms
 router.get('/', async (req, res) => {
-    await Dom.findAll().then(domList =>
-        res.send({
-            url: 'dom list',
-            status: 200,
-            domList
-        }));
+    await Dom.findAll().then(domList => res.send(domList));
 });
-
 
 module.exports = router;
