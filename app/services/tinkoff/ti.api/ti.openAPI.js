674ea205eb7e7841d0702a2b99b94071f5d91c3c
milomory <milomory@gmail.com>
2020-04-29 21:54:47 +0300

    brain start

--- app/services/tinkoff/ti.api/ti.openAPI.js
+++ app/services/tinkoff/ti.api/ti.openAPI.js
@@ -2,7 +2,7 @@
 const OpenAPI = require('@tinkoff/invest-openapi-js-sdk');
 const settings = require('../ti.settings/ti.settings').getSettings();
 
-exports.getOpenAPI = (sendbox) => {
-    if (sendbox && settings.sandboxToken)   return new OpenAPI({ apiURL: settings.sandboxApiURL, secretToken: settings.sandboxToken,    socketURL: settings.socketURL });
+exports.getOpenAPI = (sandbox) => {
+    if (sandbox && settings.sandboxToken)   return new OpenAPI({ apiURL: settings.sandboxApiURL, secretToken: settings.sandboxToken,    socketURL: settings.socketURL });
     else if (settings.secretToken)          return new OpenAPI({ apiURL: settings.apiURL, secretToken: settings.secretToken, socketURL: settings.socketURL });
 };
--- app/services/tinkoff/ti.orderBook.service.js
+++ app/services/tinkoff/ti.orderBook.service.js
@@ -1,7 +1,7 @@
 
-const api = require('./ti.api/ti.openAPI').getOpenAPI('sendbox');
+const api = require('./ti.api/ti.openAPI').getOpenAPI('sandbox');
 
 exports.getOrderBook = async (ticker, depth) => {
     const {figi} = await api.searchOne({ticker: ticker});
-    return await api.orderbookGet({figi, depth: depth});
+    return await api.orderbookGet({figi, depth: (depth)?depth:10});
 };
